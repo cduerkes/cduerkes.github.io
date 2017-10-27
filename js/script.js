dd3a37bcca4b9bd9fb0dbbc352fc8802a2abebad
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-10-27 15:26:18 +0000

    Put panel JS in script file.

--- js/script.js
+++ js/script.js
@@ -10,4 +10,42 @@ $(document).ready(function() {
   }, function() { //mouse out
     $('.satellite').animate({ left: 0, top: 0 }, 400);
   });
+
+  $(".panel-nav a#problem-solving").click(function(){
+    panels = ['.code-quality-panel', '.collaboration-panel', '.code-samples-panel']
+    $.each(panels, function(index, value) {
+      if ($(value).is(':visible')) {
+        $(value).hide('slide',{direction:'left'}, 200)};
+    }); 
+
+    $('.problem-solving-panel').show('slide',{direction:'left'}, 200);
+  });
+  $(".panel-nav a#code-quality").click(function(){
+    panels = ['.problem-solving-panel', '.collaboration-panel', '.code-samples-panel']
+    $.each(panels, function(index, value) {
+      if ($(value).is(':visible')) {
+        $(value).hide('slide',{direction:'left'}, 200)};
+    }); 
+
+    $('.code-quality-panel').show('slide',{direction:'left'}, 200);
+  });
+  $(".panel-nav a#collaboration").click(function(){
+    panels = ['.code-quality-panel', '.problem-solving-panel', '.code-samples-panel']
+    $.each(panels, function(index, value) {
+      if ($(value).is(':visible')) {
+        $(value).hide('slide',{direction:'left'}, 200)};
+    }); 
+
+    $('.collaboration-panel').show('slide',{direction:'left'}, 200);
+  });
+  $(".panel-nav a#code-samples").click(function(){
+    panels = ['.code-quality-panel', '.collaboration-panel', '.problem-solving-panel']
+    $.each(panels, function(index, value) {
+      if ($(value).is(':visible')) {
+        $(value).hide('slide',{direction:'left'}, 200)};
+    }); 
+
+    $('.code-samples-panel').show('slide',{direction:'left'}, 200);
+  });
+
 });
