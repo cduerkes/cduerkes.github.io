eea63185211c786ae803097c477a55fa1b1ad961
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-10-28 17:04:10 +0000

    Edit og:image, panel slide direction and header.

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     panels = ['.code-quality-panel', '.collaboration-panel', '.code-samples-panel']
     $.each(panels, function(index, value) {
       if ($(value).is(':visible')) {
-        $(value).hide('slide',{direction:'left'}, 200)};
+        $(value).hide('slide',{direction:'right'}, 100)};
     }); 
 
     $('.problem-solving-panel').show('slide',{direction:'left'}, 200);
@@ -24,28 +24,28 @@ $(document).ready(function() {
     panels = ['.problem-solving-panel', '.collaboration-panel', '.code-samples-panel']
     $.each(panels, function(index, value) {
       if ($(value).is(':visible')) {
-        $(value).hide('slide',{direction:'left'}, 200)};
+        $(value).hide('slide',{direction:'right'}, 200)};
     }); 
 
-    $('.code-quality-panel').show('slide',{direction:'left'}, 200);
+    $('.code-quality-panel').show('slide',{direction:'left'}, 100);
   });
   $(".panel-nav a#collaboration").click(function(){
     panels = ['.code-quality-panel', '.problem-solving-panel', '.code-samples-panel']
     $.each(panels, function(index, value) {
       if ($(value).is(':visible')) {
-        $(value).hide('slide',{direction:'left'}, 200)};
+        $(value).hide('slide',{direction:'right'}, 200)};
     }); 
 
-    $('.collaboration-panel').show('slide',{direction:'left'}, 200);
+    $('.collaboration-panel').show('slide',{direction:'left'}, 100);
   });
   $(".panel-nav a#code-samples").click(function(){
     panels = ['.code-quality-panel', '.collaboration-panel', '.problem-solving-panel']
     $.each(panels, function(index, value) {
       if ($(value).is(':visible')) {
-        $(value).hide('slide',{direction:'left'}, 200)};
+        $(value).hide('slide',{direction:'right'}, 200)};
     }); 
 
-    $('.code-samples-panel').show('slide',{direction:'left'}, 200);
+    $('.code-samples-panel').show('slide',{direction:'left'}, 100);
   });
 
 });
