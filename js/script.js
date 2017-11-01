c8eb0121fe89fd1f15c0fd99d727994db4147c5c
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-11-01 22:08:06 +0000

    Add slide in from bottom.

--- js/script.js
+++ js/script.js
@@ -64,22 +64,56 @@ $(document).ready(function() {
                 left: -107
             }, 400);
     });
+  (function($) {
 
-  function fade() {
-    $('.fade').each(function() {
-        /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
-        var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-        
-        /* If the object is completely visible in the window, fade it in */
-        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-            if ($(this).css('opacity')==0) {$(this).fadeTo(200,1);}
-        } else { //object goes out of view (scrolling up)
-            if ($(this).css('opacity')==1) {$(this).fadeTo(200,0);}
-        }
-    });
-  }
-  fade(); //Fade in completely visible elements during page-load
-  $(window).scroll(function() {fade();}); //Fade in elements during scroll
+      /**
+       * Copyright 2012, Digital Fusion
+       * Licensed under the MIT license.
+       * http://teamdf.com/jquery-plugins/license/
+       *
+       * @author Sam Sehnert
+       * @desc A small plugin that checks whether elements are within
+       *     the user visible viewport of a web browser.
+       *     only accounts for vertical position, not horizontal.
+       */
+
+      $.fn.visible = function(partial) {
+
+          var $t = $(this),
+              $w = $(window),
+              viewTop = $w.scrollTop(),
+              viewBottom = viewTop + $w.height(),
+              _top = $t.offset().top,
+              _bottom = _top + $t.height(),
+              compareTop = partial === true ? _bottom : _top,
+              compareBottom = partial === true ? _top : _bottom;
+
+          return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+      };
+
+  })(jQuery);
+
+  var win = $(window);
+
+  var allMods = $(".slide-box");
+
+  allMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+          el.addClass("already-visible");
+      }
+  });
+
+  win.scroll(function(event) {
+
+      allMods.each(function(i, el) {
+          var el = $(el);
+          if (el.visible(true)) {
+              el.addClass("come-in");
+          }
+      });
+
+  });
 
 });
