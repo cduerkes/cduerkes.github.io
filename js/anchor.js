a0ec375de8ee6c2c1a388939db95c7ab470b0831
Christopher Duerkes <christopher.duerkes@gmail.com>
2018-12-28 23:42:29 -0500

    Update skills feature. Layout intial about section.

--- js/anchor.js
+++ js/anchor.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
 
   $('a[rel="relativeanchor"]').click(function(){
       $('html, body').animate({
-          scrollTop: $( $.attr(this, 'href') ).offset().top - 150
+          scrollTop: $( $.attr(this, 'href') ).offset().top - 175
       }, 1000);
+
+      $('.navigation__button').click();
       return false;
   }); 
 
--- js/script.js
+++ js/script.js
@@ -1,27 +1,21 @@
 $(document).ready(function() {
 
-  $('a[rel="relativeanchor"]').click(function(){
-      $('html, body').animate({
-          scrollTop: $( $.attr(this, 'href') ).offset().top - 150
-      }, 1000);
-
-      $('.navigation__button').click();
-      return false;
-  }); 
-
   var controller = new ScrollMagic.Controller();
 
-  var skillTween = new TweenMax.to('.section__skill--HTML5', 1, {
-      color: 'red',
-      scale: 1.1
+  $(".section__skill").each(function(i, val) {
+    var skillTween = new TweenMax.to(this, 2, {
+        opacity: .7,
+        scale: .90
+    });
+
+    var scene = new ScrollMagic.Scene({
+      triggerElement: this,
+      duration: 250
+    })
+    .triggerHook(".5")
+    .setPin(`.pin-${i}`)
+    .setTween(skillTween)
+//    .addIndicators()
+    .addTo(controller);
   });
-
-  var scene = new ScrollMagic.Scene({
-  triggerElement: '.section__skill' // point of execution
-})
-  .triggerHook(".5")
-  .setTween(skillTween)
-  .addIndicators()
-  .addTo(controller);
-
 });
