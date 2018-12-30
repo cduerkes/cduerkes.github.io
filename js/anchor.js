fd613c0f642caca7a206ebb4a8db55ba0c72f441
Christopher Duerkes <christopher.duerkes@gmail.com>
2018-12-29 23:06:28 -0500

    Fine tune colors and design.

--- js/anchor.js
+++ js/anchor.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
 
   $('a[rel="relativeanchor"]').click(function(){
+      const anchor= $.attr(this, 'href');
+      let offset;
+
+      if (anchor === '#home-anchor') {
+        scrollTo = 0;
+      } else {
+        scrollTo = $( $.attr(this, 'href') ).offset().top - 75;
+      }
+
       $('html, body').animate({
-          scrollTop: $( $.attr(this, 'href') ).offset().top - 175
+          scrollTop: scrollTo
       }, 1000);
 
       $('.navigation__button').click();
--- js/script.js
+++ js/script.js
@@ -3,19 +3,65 @@ $(document).ready(function() {
   var controller = new ScrollMagic.Controller();
 
   $(".section__skill").each(function(i, val) {
-    var skillTween = new TweenMax.to(this, 2, {
-        opacity: .7,
-        scale: .90
+    var skillTween2 = new TweenMax.to(this, 8, {
+        opacity: .5,
+        scale: .90,
+        ease: Cubic.easeOut
     });
 
-    var scene = new ScrollMagic.Scene({
+    var skillTween1 = new TweenMax.to(this, .5, {
+        opacity: 1,
+        scale: 1.1,
+        ease: Cubic.easeOut
+    });
+
+    var scene1 = new ScrollMagic.Scene({
       triggerElement: this,
-      duration: 250
+      triggerHook: 'onEnter',
+      offset: 300,
+      duration: 300
     })
-    .triggerHook(".5")
-    .setPin(`.pin-${i}`)
-    .setTween(skillTween)
+    .setTween(skillTween1)
+//    .addIndicators()
+    .addTo(controller);
+
+    var scene2 = new ScrollMagic.Scene({
+      triggerElement: this,
+      duration: 300
+    })
+    .triggerHook(".2")
+    .setTween(skillTween2)
 //    .addIndicators()
     .addTo(controller);
   });
+
+  var bg_tween1 = TweenMax.to('.section, footer', 1, {
+    backgroundColor: '#ADD8E6',  //#57599f (purple-blue) #ff6347(tomato)
+    ease: Linear.easeNone
+  });
+
+  var bg_tween2 = TweenMax.to('.section, footer', 1, {
+    backgroundColor: '#ff6347',  //#57599f (purple-blue) #ff6347(tomato) #ff6347
+    ease: Linear.easeNone
+  });
+
+  var bg_scene1 = new ScrollMagic.Scene({
+    triggerElement: '.section--4',
+    duration: 300
+  })
+  .triggerHook("1")
+  .setTween(bg_tween1);
+
+  var bg_scene2 = new ScrollMagic.Scene({
+    triggerElement: 'footer',
+    duration: 300
+  })
+  .triggerHook("1")
+  .setTween(bg_tween2);
+
+  controller.addScene([
+    bg_scene1,
+    bg_scene2
+  ]);
+
 });
