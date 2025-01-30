cab5c7527bb1faa87bbdc4688b987fdbb1d98d77
cduerkes <christopher.duerkes@gmail.com>
2025-01-30 12:22:33 -0700

    Update resume, modernize

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ $(document).ready(function() {
     })
     .triggerHook(".2")
     .setTween(skillTween1)
-//    .addIndicators()
     .addTo(controller);
 
     var scene2 = new ScrollMagic.Scene({
@@ -31,50 +30,22 @@ $(document).ready(function() {
       duration: 300
     })
     .setTween(skillTween2)
-//    .addIndicators()
     .addTo(controller);
   });
 
-  var bg_tween0 = TweenMax.to('.section, footer', 1, {
-    backgroundColor: 'rgb(0, 3, 111)', //about
-    ease: Linear.easeNone
-  });
+  // var bg_tween0 = TweenMax.to('.section, footer', 1, {
+  //   backgroundColor: 'rgb(132, 151, 26)', //about
+  //   ease: Linear.easeNone
+  // });
 
-  var bg_tween1 = TweenMax.to('.section, footer', 1, {
-    backgroundColor: 'rgb(253, 200, 8)', //experience
-    ease: Linear.easeNone
-  });
+  // var bg_scene0 = new ScrollMagic.Scene({
+  //   triggerElement: '.section--3', //about
+  //   triggerHook: 'onEnter',
+  //   duration: 300
+  // })
+  // .setTween(bg_tween0);
 
-  var bg_tween2 = TweenMax.to('.section, footer', 1, {
-    backgroundColor: 'rgb(132, 151, 26)', //contact
-    ease: Linear.easeNone
-  });
-
-  var bg_scene0 = new ScrollMagic.Scene({
-    triggerElement: '.section--3', //about
-    triggerHook: 'onEnter',
-    duration: 300
-  })
-  .setTween(bg_tween0);
-
-  var bg_scene1 = new ScrollMagic.Scene({
-    triggerElement: '.section--4', //experience
-    duration: 300
-  })
-  .triggerHook("1")
-  .setTween(bg_tween1);
-
-  var bg_scene2 = new ScrollMagic.Scene({
-    triggerElement: 'footer',
-    duration: 300
-  })
-  .triggerHook("1")
-  .setTween(bg_tween2);
-
-  controller.addScene([
-    bg_scene0,
-    bg_scene1,
-    bg_scene2
-  ]);
-
+  // controller.addScene([
+  //   bg_scene0,
+  // ]);
 });
