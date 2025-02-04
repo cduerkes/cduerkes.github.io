a5706aa84f7f709ce27b0c128d58362f5dd52b96
cduerkes <christopher.duerkes@gmail.com>
2025-02-04 07:58:04 -0700

    Updates, prettier

--- js/anchor.js
+++ js/anchor.js
@@ -1,23 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $('a[rel="relativeanchor"]').click(function () {
+    const anchor = $.attr(this, 'href');
+    let offset;
 
-  $('a[rel="relativeanchor"]').click(function(){
-      const anchor= $.attr(this, 'href');
-      let offset;
+    if (anchor === '#home-anchor') {
+      scrollTo = 0;
+    } else if (anchor === '#experience-anchor') {
+      scrollTo = $($.attr(this, 'href')).offset().top - 150;
+    } else {
+      scrollTo = $($.attr(this, 'href')).offset().top - 185;
+    }
 
-      if (anchor === '#home-anchor') {
-        scrollTo = 0;
-      } else if (anchor === '#experience-anchor') {
-        scrollTo = $( $.attr(this, 'href') ).offset().top - 150;
-      } else {
-        scrollTo = $( $.attr(this, 'href') ).offset().top - 185;
-      }
+    $('html, body').animate(
+      {
+        scrollTop: scrollTo,
+      },
+      1000
+    );
 
-      $('html, body').animate({
-          scrollTop: scrollTo
-      }, 1000);
-
-      $('.navigation__button').click();
-      return false;
-  }); 
-
+    $('.navigation__button').click();
+    return false;
+  });
 });
--- js/script.js
+++ js/script.js
@@ -1,36 +1,35 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   var controller = new ScrollMagic.Controller();
 
-  $(".section__skill").each(function(i, val) {
+  $('.section__skill').each(function (i, val) {
     var skillTween1 = new TweenMax.to(this, 8, {
-        opacity: 1, //.7
-        scale: .90,
-        ease: Cubic.easeOut
+      opacity: 1, //.7
+      scale: 0.9,
+      ease: Cubic.easeOut,
     });
 
-    var skillTween2 = new TweenMax.to(this, .5, {
-        opacity: 1,
-        scale: 1.1,
-        ease: Cubic.easeOut
+    var skillTween2 = new TweenMax.to(this, 0.5, {
+      opacity: 1,
+      scale: 1.1,
+      ease: Cubic.easeOut,
     });
 
     var scene1 = new ScrollMagic.Scene({
       triggerElement: this,
-      duration: 300
+      duration: 300,
     })
-    .triggerHook(".2")
-    .setTween(skillTween1)
-    .addTo(controller);
+      .triggerHook('.2')
+      .setTween(skillTween1)
+      .addTo(controller);
 
     var scene2 = new ScrollMagic.Scene({
       triggerElement: this,
       triggerHook: 'onEnter',
       offset: 300,
-      duration: 300
+      duration: 300,
     })
-    .setTween(skillTween2)
-    .addTo(controller);
+      .setTween(skillTween2)
+      .addTo(controller);
   });
 
   // var bg_tween0 = TweenMax.to('.section, footer', 1, {
